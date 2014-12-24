3946ccf85080eae82eb0c8892159e96223e3cee0
Romack Natividad <romacknatividad@gmail.com>
2014-12-24 13:57:33 +0800

    Converted page to AngularJS. added controllers.js and attendance.js

---
+++ web/assets/js/attendance.js
@@ -0,0 +1,182 @@
+var places = [
+	{
+		district: "Central",
+		zone: 1,
+	    name: "Group 1",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 1,
+	    name: "Group 2",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 1,
+	    name: "Group 3",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 1,
+	    name: "Group 4",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 1,
+	    name: "Group 5",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 1,
+	    name: "Group 6",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 1,
+	    name: "Group 7",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 2,
+	    name: "Capalangan",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 2,
+	    name: "Old Central",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 2,
+	    name: "San Juan",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 2,
+	    name: "San Juan",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 2,
+	    name: "San Nicholas",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 2,
+	    name: "San Simon",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 2,
+	    name: "Sulipan",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 2,
+	    name: "Sto. Domingo",
+	    visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 3,
+		name: "Alauli",
+		visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 3,
+		name: "Dalaquitan",
+		visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 3,
+		name: "Palimpe",
+		visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: "Central",
+		zone: 3,
+		name: "San Esteban",
+		visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	},
+	{
+		district: false,
+		zone: false,
+		name: "Other Division",
+		visible: true,
+	    guests: 0,
+	    brethren: 0,
+	    sub_total: 0
+	}
+];
